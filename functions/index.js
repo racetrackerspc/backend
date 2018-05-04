95ca4ed7381ad7fdf4a505b4e76d332e0033a6de
Adrian Martin <adrianmartinparra@gmail.com>
2018-05-04 19:50:54 +0000

    Create leaderboard from participants list

--- functions/index.js
+++ functions/index.js
@@ -2,10 +2,10 @@
 const functions = require('firebase-functions');
 const bigquery = require('@google-cloud/bigquery')();
 const admin = require('firebase-admin');
-const gcs = require('@google-cloud/storage')();
 
 admin.initializeApp();
 
+const nearestPointOnLine = require('@turf/nearest-point-on-line').default;
 const tj = require('@mapbox/togeojson');
 const _ = require('lodash');
 
@@ -14,21 +14,22 @@ const path = require('path');
 const fs = require('fs');
 const os = require('os');
 
-const endpoint = '/participants_test';
+const config = functions.config();
 const db = admin.database();
+
+let raceTrack;
 
 
 function updateFirebase(data) {
   if (data.properties.longitude) {
-    return db.ref(endpoint).child(data.properties.deviceId).set(data);
-  } else {
-    return null
+    return db.ref(config.paths.participants).child(data.properties.deviceId).set(data);
   }
+  return null;
 }
 
 function addToBigquery(data) {
-  const dataset = bigquery.dataset(functions.config().bigquery.datasetname);
-  const table = dataset.table(functions.config().bigquery.tablename);
+  const dataset = bigquery.dataset(config.bigquery.datasetname);
+  const table = dataset.table(config.bigquery.tablename);
 
   return table.insert(data);
 }
@@ -37,6 +38,21 @@ function sameLocation(oldPayload, newPayload) {
   return (oldPayload.longitude === newPayload.longitude) &&
          (oldPayload.latitude === newPayload.latitude);
 }
+
+
+function snapParticipants(participants) {
+  let leaderboard = {};
+
+  for (const id in participants) {
+    let snap = nearestPointOnLine(raceTrack, participants[id]);
+
+    Object.assign(snap.properties, participants[id].properties);
+    leaderboard[id] = snap;
+  }
+
+  return leaderboard;
+}
+
 
 exports.saveDeviceData = functions.https.onRequest((req, res) => {
   const rawData = req.body;
@@ -65,7 +81,7 @@ exports.saveDeviceData = functions.https.onRequest((req, res) => {
 
   payload = JSON.parse(JSON.stringify(payload));
 
-  db.ref(endpoint).child(deviceId).once("value", data => {
+  db.ref(config.paths.participants).child(deviceId).once('value', data => {
     let oldGeojson = data.val();
 
     if (oldGeojson) {
@@ -97,38 +113,65 @@ exports.saveDeviceData = functions.https.onRequest((req, res) => {
     return Promise.all([
       updateFirebase(geojson),
       addToBigquery(payload)
-    ]).then(() => { return res.status(200).send({ status: 'OK' });
-    });
+    ])
+    .then(() =>  res.status(200).send({ status: 'OK' }));
   });
+});
+
+
+exports.updateLeaderboard = functions.https.onRequest((req, res) => {
+  const filePath = req.body.filePath;
+  const fileName = path.basename(filePath);
+
+  const tempFilePath = path.join(os.tmpdir(), fileName);
+  const bucket = admin.storage().bucket();
+
+  let participants;
+
+  db.ref(config.paths.participants)
+  .once('value', data => participants = data.val())
+  .then(() => {
+    if (!raceTrack) {
+      console.log(`Loading "${fileName}"`);
+      return bucket.file(filePath).download({ destination: tempFilePath })
+      .then(() => raceTrack = JSON.parse(fs.readFileSync(tempFilePath, 'utf8')))
+      .then(() => fs.unlinkSync(tempFilePath));
+    }
+    console.log(`"${fileName}" already loaded`);
+    return;
+  })
+  .then(() => snapParticipants(participants))
+  .then(leaderboard => db.ref(config.paths.leaderboard).set(leaderboard))
+  .then(() => res.status(200).send({ status: 'OK' }));
 });
 
 
 exports.generateGeoJSON = functions.storage.object().onFinalize((object) => {
   const contentType = object.contentType;
-  const fileBucket = object.bucket;
   const filePath = object.name;
   const fileName = path.basename(filePath);
 
   if (!fileName.endsWith('.gpx')) {
-    console.log(`'${fileName}' is not a GPX file.`);
+    console.log(fileName, 'is not a GPX file');
     return null;
   }
 
   const tempFilePath = path.join(os.tmpdir(), fileName);
   const metadata = { contentType: contentType };
-  const bucket = gcs.bucket(fileBucket);
+  const bucket = admin.storage().bucket();
 
-  return bucket.file(filePath).download({
-    destination: tempFilePath
-  }).then(() => {
+  return bucket.file(filePath).download({ destination: tempFilePath })
+  .then(() => {
     console.log('GPX file downloaded locally to', tempFilePath);
 
-    let gpx = new DOMParser().parseFromString(fs.readFileSync(tempFilePath, 'utf8'));
+    let content = fs.readFileSync(tempFilePath, 'utf8');
+    let gpx = new DOMParser().parseFromString(content);
     let geo = tj.gpx(gpx);
 
     return fs.writeFileSync(tempFilePath, JSON.stringify(geo));
-  }).then(() => {
-    const geoJSONfileName = _.replace(fileName, '.gpx', '.geojson');
+  })
+  .then(() => {
+    const geoJSONfileName = fileName.replace('.gpx', '.geojson');
     const geoJSONfilePath = path.join(path.dirname(filePath), geoJSONfileName);
 
     console.log('GeoJSON file created at', geoJSONfilePath);
@@ -137,5 +180,6 @@ exports.generateGeoJSON = functions.storage.object().onFinalize((object) => {
       destination: geoJSONfilePath,
       metadata: metadata
     });
-  }).then(() => fs.unlinkSync(tempFilePath));
+  })
+  .then(() => fs.unlinkSync(tempFilePath));
 });
